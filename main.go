adabd35bbfef05e92e262740e7aa910cfb7f8370
riobagaspamungkas <riobagasp24@gmail.com>
2023-03-12 10:00:25 +0700

    test commit

--- main.go
+++ main.go
@@ -8,15 +8,4 @@ func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println("Nilai i =", i)
 	}
-
-	for j := 0; j < 11; j++ {
-		if j == 5 {
-			characters := []rune{'\u0421', '\u0410', '\u0428', '\u0410', '\u0420', '\u0412', '\u041E'}
-			for pos, character := range characters {
-				fmt.Printf("character %U '%c' starts at byte position %d \n", character, character, pos*2)
-			}
-		} else {
-			fmt.Println("Nilai j =", j)
-		}
-	}
 }
